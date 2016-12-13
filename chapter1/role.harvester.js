e5b3bf3300b724a5c2c59e57e6c3e7a2e9300e1c
Corey Kozlowski <ckreon@gmail.com>
2016-12-13 03:53:01 -0800

    Adjust changes

--- chapter1/role.harvester.js
+++ chapter1/role.harvester.js
@@ -25,16 +25,24 @@ var roleHarvester = {
 			}
 		}
 		else {
-			var targets = creep.room.find(FIND_STRUCTURES, {
+			var targets = creep.pos.findInRange(FIND_STRUCTURES, 1, {
 				filter: (structure) => {
-					return (structure.structureType == STRUCTURE_SPAWN);
+					return (structure.structureType == STRUCTURE_CONTAINER);
 				}
 			});
 			
-			if (targets.length > 0) {
-				if (creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-					creep.moveTo(targets[0]);
-				}
+			if (targets.length == 0) {
+				var targets = creep.room.find(FIND_STRUCTURES, {
+					filter: (structure) => {
+						return (
+							((structure.structureType == STRUCTURE_SPAWN) ||
+							 (structure.structureType == STRUCTURE_EXTENSION)) &&
+							(structure.energy < structure.energyCapacity));
+					}
+				});
+			}
+			if (creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+				creep.moveTo(targets[0]);
 			}
 			if (creep.carry.energy == 0) {
 				creep.say('Harvesting');
