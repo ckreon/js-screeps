b404f322a5056a6dd9883be6be75fa23bb54f1d0
Corey Kozlowski <ckreon@gmail.com>
2016-12-13 01:41:47 -0800

    Fix typo

--- chapter1/ai.spawning.js
+++ chapter1/ai.spawning.js
@@ -52,7 +52,7 @@ module.exports.upgraders = function() {
 	if (upgrader_count.length < 1) {
 		var newName = Game.spawns['Spawn1'].createCreep(
 			[WORK, WORK, CARRY, MOVE],
-			undefined, { role: 'upgrader', harveesting: 'true' });
+			undefined, { role: 'upgrader', harvesting: 'true' });
 
 		console.log('Spawning new Upgrader: ' + newName);
 	}
