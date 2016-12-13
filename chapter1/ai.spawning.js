0b5602adfdb6237812a2a8f729fcd2b58c68470b
Corey Kozlowski <ckreon@gmail.com>
2016-12-13 03:16:26 -0800

    Adjust changes

--- chapter1/ai.spawning.js
+++ chapter1/ai.spawning.js
@@ -13,7 +13,7 @@ module.exports.builders = function() {
 module.exports.harvesters = function() {
 	var harvester_count = _.filter(Game.creeps, (creep) =>
 													(creep.memory.role == 'harvester'));
-	if (harvester_count.length < 4) {
+	if (harvester_count.length < 3) {
 		var newName = Game.spawns['Spawn1'].createCreep(
 			[WORK, CARRY, CARRY, MOVE, MOVE],
 			undefined, { role: 'harvester', harvesting: 'true' });
