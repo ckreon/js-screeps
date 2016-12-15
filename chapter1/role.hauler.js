114113fce7ebe14301507e170b197ecb942c2141
Corey Kozlowski <ckreon@gmail.com>
2016-12-15 11:57:05 -0800

    Minor changes

--- chapter1/role.hauler.js
+++ chapter1/role.hauler.js
@@ -32,16 +32,9 @@ var roleHauler = {
 			var targets = creep.room.find(FIND_STRUCTURES, {
 				filter: (structure) => {
 					return (
-							 (((structure.structureType == STRUCTURE_SPAWN) ||
+								((structure.structureType == STRUCTURE_SPAWN) ||
 								 (structure.structureType == STRUCTURE_EXTENSION)) &&
-								 (structure.energy < structure.energyCapacity))
-								||
-								((structure.structureType == STRUCTURE_TOWER) && 
-								 (structure.energy < structure.energyCapacity - 200))
-								||
-								((structure.structureType == STRUCTURE_CONTAINER) &&
-								 (_.sum(structure.store) < structure.storeCapacity) &&
-								 (structure.pos.findInRange(FIND_SOURCES, 2).length == 0)));
+								 (structure.energy < structure.energyCapacity));
 				}
 			});
 
@@ -59,7 +52,7 @@ var roleHauler = {
 				}
 			});
 			
-			if (targets.length > 0) {
+			if (targets.length) {
 				if (creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
 					creep.moveTo(targets[0]);
 				}
