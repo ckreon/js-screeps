410d3747768acd7d89fd20bcba7f4503a241f88e
Corey Kozlowski <ckreon@gmail.com>
2016-12-15 13:03:12 -0800

    Minor changes

--- chapter1/role.harvester.js
+++ chapter1/role.harvester.js
@@ -4,25 +4,29 @@ var roleHarvester = {
 	run: function(creep) {
 
 		if (creep.memory.harvesting) {
-			var sources = creep.room.find(FIND_SOURCES);
 			var energy = creep.pos.findInRange(FIND_DROPPED_ENERGY, 3);
 
-			if (energy.length) {
-				creep.pickup(energy[0]);
-			}
+			if (energy.length == 0) {
+				var sources = creep.room.find(FIND_SOURCES);
 
-			if (!creep.memory.source) {
-				for (source in sources) {
-					if (_.filter(Game.creeps, (creep) =>
-							(creep.memory.role == 'harvester') &&
-							(creep.memory.source == source)
-					).length < 2) {
-						creep.memory.source = source;
+				if (!creep.memory.source) {
+					for (source in sources) {
+						if (_.filter(Game.creeps, (creep) =>
+							 	 				(creep.memory.role == 'harvester') &&
+							 	 				(creep.memory.source == source)
+							 					).length < 2) {
+							creep.memory.source = source;
+						}
 					}
 				}
+				if (creep.harvest(sources[creep.memory.source]) == ERR_NOT_IN_RANGE) {
+					creep.moveTo(sources[creep.memory.source]);
+				}
 			}
-			if (creep.harvest(sources[creep.memory.source]) == ERR_NOT_IN_RANGE) {
-				creep.moveTo(sources[creep.memory.source]);
+			else {
+				if (creep.pickup(energy[0]); == ERR_NOT_IN_RANGE) {
+					creep.moveTo(energy[0]);
+				}
 			}
 			if (creep.carry.energy == creep.carryCapacity) {
 				creep.say('Depositing');
@@ -30,7 +34,7 @@ var roleHarvester = {
 			}
 		}
 		else {
-			var targets = creep.pos.findInRange(FIND_STRUCTURES, 3, {
+			var targets = creep.pos.findInRange(FIND_STRUCTURES, 2, {
 				filter: (structure) => {
 					return (structure.structureType == STRUCTURE_CONTAINER);
 				}
