834dd7ab8549b5e7352479fa7dfe82c1f3be24bd
Corey Kozlowski <ckreon@gmail.com>
2016-12-16 03:31:33 -0800

    Minor changes

--- chapter1/ai.spawn.js
+++ chapter1/ai.spawn.js
@@ -1,7 +1,7 @@
 // SET UNIT NUMBERS HERE
 var builders_wanted = 3;
 var harvesters_wanted = 2;
-var haulers_wanted = 3;
+var haulers_wanted = 4;
 var healers_wanted = 1;
 var upgraders_wanted = 4;
 
