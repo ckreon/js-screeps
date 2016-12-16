b9bc2cda169dece5ffeab4b392422bed126d8e55
Corey Kozlowski <ckreon@gmail.com>
2016-12-15 16:48:29 -0800

    Minor changes

--- chapter1/role.builder.js
+++ chapter1/role.builder.js
@@ -11,7 +11,7 @@ var roleBuilder = {
 				filter: (structure) => {
 					return (
 								 (structure.structureType == STRUCTURE_SPAWN) &&
-								 (Game.spawns.Spawn1.room.energyAvailable > 550));
+								 (Game.spawns.Spawn1.room.energyAvailable > 500));
 				}
 			});
 
