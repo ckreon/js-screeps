c9df7dab3d34b779183d1a88d3a3ee2dd76a34ab
Corey Kozlowski <ckreon@gmail.com>
2016-12-16 04:37:42 -0800

    Minor changes

--- chapter1/role.upgrader.js
+++ chapter1/role.upgrader.js
@@ -4,39 +4,32 @@ var roleUpgrader = {
 	run: function(creep) {
 
 		if (creep.memory.harvesting) {
-			var energy = creep.pos.findInRange(FIND_DROPPED_ENERGY, 3);
-
-			if (energy.length == 0) {
-				var storage = creep.room.find(FIND_STRUCTURES, {
-					filter: (structure) => {
-						return (
-							(structure.structureType == STRUCTURE_CONTAINER) &&
-							(structure.store[RESOURCE_ENERGY] > creep.carryCapacity));
-					}
-				});
-
-				if (storage.length == 0) {
-					var sources = creep.room.find(FIND_SOURCES);
-					creep.memory.source = 0;
-
-					if (creep.harvest(sources[creep.memory.source]) == ERR_NOT_IN_RANGE) {
-					creep.moveTo(sources[creep.memory.source]);
-					}
+			var storage = creep.room.find(FIND_STRUCTURES, {
+				filter: (structure) => {
+					return (
+						(structure.structureType == STRUCTURE_CONTAINER) &&
+						(structure.store[RESOURCE_ENERGY] > creep.carryCapacity));
 				}
-				else {
-					var target = creep.pos.findClosestByRange(storage);
+			});
 
-					if (!creep.pos.isNearTo(target)) {
-						creep.moveTo(target);
-					}
-					else {
-						creep.withdraw(target, RESOURCE_ENERGY,
-													(creep.carryCapacity - _.sum(creep.carry)));
-					}
+			if (storage.length == 0) {
+				var sources = creep.room.find(FIND_SOURCES);
+				creep.memory.source = 0;
+
+				if (creep.harvest(sources[creep.memory.source]) == ERR_NOT_IN_RANGE) {
+				creep.moveTo(sources[creep.memory.source]);
 				}
 			}
 			else {
-				creep.pickup(energy[0]);
+				var target = creep.pos.findClosestByRange(storage);
+
+				if (!creep.pos.isNearTo(target)) {
+					creep.moveTo(target);
+				}
+				else {
+					creep.withdraw(target, RESOURCE_ENERGY,
+												(creep.carryCapacity - _.sum(creep.carry)));
+				}
 			}
 			if (creep.carry.energy == creep.carryCapacity) {
 				creep.say('Upgrading');
